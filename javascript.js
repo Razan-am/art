3ab4c96c7cba07228a308a07115dcd4df7b7d0f4
Razanam <>
2021-04-14 13:37:53 +0000

    updated

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+var main = ('welocme')
+alert (main);
+
+var user = prompt ('please enter the name of the page (login,products,ourteam,artistic)');
+
+if (user == 'artistic'){
+  var image = ('<img src="https://i.pinimg.com/236x/34/86/e8/3486e84bc958e1bf5cb22ebdc75042b3.jpg" alt="i" class="image">');
+  
+  document.write ('image');
+}
+else {
+  console.log ('false');
+}
+
+
+
+let result = confirm('Are you sure you wanna leave this page?');
+
