6455a6177ec9ed9ae027e1ad6e6f91b38069eb16
Razanam <>
2021-04-15 12:07:42 +0000

    updated

--- javascript.js
+++ javascript.js
@@ -2,12 +2,12 @@
 var main = ('welocme')
 alert (main);
 
-var user = prompt ('please enter the name of the page (login,products,ourteam,artistic)');
+var user = prompt ('please enter the name of the page (artistic)');
 
 if (user == 'artistic'){
-  var image = ('<img src="https://i.pinimg.com/236x/34/86/e8/3486e84bc958e1bf5cb22ebdc75042b3.jpg" alt="i" class="image">');
+    var image = '<img src="https://assets.awwwards.com/awards/screenshots/2014/06/5391af8c2c382.jpeg" alt= "invitation"/>';
   
-  document.write ('image');
+  document.write (image);
 }
 else {
   console.log ('false');
@@ -15,5 +15,35 @@ else {
 
 
 
-let result = confirm('Are you sure you wanna leave this page?');
+var login = prompt ('please enter A1 to log in');
+
+while (login !== 'A1'){
+
+  login = prompt ('please enter A1 to log in');
+
+}
+
+
+
+var ticketsNum = prompt ('How many tickets you want to reserve?');
+
+for (var i=1 ; i<=ticketsNum ; i++){
+
+if (user == 'artistic'){
+    var image = '<img src="https://i.pinimg.com/236x/c2/bc/b1/c2bcb1d40b7d834bb0e85cced7ed2447.jpg" alt= "invitation"/>';
+  
+  document.write (image);
+}
+else {
+  console.log ('false');
+}
+
+
+}
+
+
+
+
+let result = confirm('Please enter on the tickets to finish the reservation  ?');
+
 
